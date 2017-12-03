1014d1f948f468350f3acec1dfcd266b41572f86
Daniel Beck <nowseemee@gmail.com>
2017-12-03 13:16:50 +0100

    add clickHandler and presentational components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import Input from './Input'
+import Button from './Button'
+import Feedback from './Feedback'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    status: ''
+  };
+
+  handleClick = () => {
+    if (!this.input.value) {
+      return this.setState({ hasError: true });
+    }
+    return fetch('/create', {
+      method: 'post',
+      body: JSON.stringify({ original: this.input.value }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => this.setState({ data }))
+    .catch(() => this.setState({ hasError: true }))
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +37,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Feedback {...this.state} />
+        <Input inputRef={(ref) => { this.input = ref } } />
+        <Button onClick={this.handleClick} />
       </div>
     );
   }
